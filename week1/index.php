c61904d47856b3740afadcd33ee6d4664a292b29
Brown6734 <clbrown1@email.neit.edu>
2024-03-25 09:03:02 -0400

    first php class spring 2024 working on week 1 assignment Brown

---
+++ week1/index.php
@@ -0,0 +1,26 @@
+<!--<!DOCTYPE html>-->
+<!--<html>-->
+    <!--<head>-->
+        <!--<meta charset="UTF-8">-->
+    <!--</head>-->
+    <!--<body>-->
+            <!--<h1>SE 266 - Hello World</h1>-->
+
+        <?php
+
+        //$greeting = 'Hello Universe';
+
+        //$name = "Chloe Brown";
+
+        //echo "Hello, $name";
+
+        $greeting = 'Hello';
+
+        $name = 'Chloe Brown';
+
+        echo "{$greeting}, {$name}";
+        ?>
+    <!--</body>-->
+    
+
+<!--</html>-->
