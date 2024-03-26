cd8cf53b2873db04a98725e99e720076f460a999
Brown6734 <clbrown1@email.neit.edu>
2024-03-26 09:25:37 -0400

    Brown completed w1 task C spring 2024

--- week1/index.view.php
+++ week1/index.view.php
@@ -2,10 +2,11 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Week 1 Assignment Brown</title>
+        <title>Brown: Week 1 Task C</title>
 
         <style>
 
+            /*below is some styling for the HTML header*/
             header{
                 background: #e3e3e3;
                 padding: 2em;
@@ -18,6 +19,10 @@
     
             <ul>
 
+                <!--below: 
+                    is a way to call the animals array from the index.php file
+
+                -->
                 <?php foreach ($animals as $animal) : ?>
                    
                    <li><?= $animal; ?></li>
