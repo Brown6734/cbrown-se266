46c4892c3bf27a1bb2e28caf2fb0b901354c86e7
Brown6734 <clbrown1@email.neit.edu>
2024-04-12 11:10:04 -0400

    Brown spring 2024; completed week 2 site assignment.

--- site/index.php
+++ site/index.php
@@ -26,12 +26,6 @@
 <body>
 
     <h1>Chloe Brown: My Site </h1>
-    
-    <!-- dd -->
-
-    <!--<h1>Chloe Brown; my site </h1>-->
-
-<!--<h2>smaller heading here </h2>-->
 
  <?php
 
@@ -50,58 +44,10 @@
     //links to your working lab solutions
         //for now, create placeholders for weeks 3 through 10
 
-    
-    //3 links to stuff you enjoy doing outside of learning PHP or Git below
-    //https://store.steampowered.com/app/1629520/A_Little_to_the_Left/    - A Little to the Left
-
-    //Cozy Puzzle/Organization Game
-
-    //https://store.steampowered.com/app/2367190/Looks_Good/      - Looks Good
-
-    //Casual Puzzle Game
-
-    //https://store.steampowered.com/app/702670/Donut_County/      - Donut County
-    //http://donutcounty.com/
-    //https://store.steampowered.com/app/702670/Donut_County/
-
-    //game where you swallow things with a hole in the ground
-
-
-
-    //3 links to resources that you can use to learn PHP below
-    //https://www.php.net/manual/en/      - PHP English Manual
-    //https://www.php.net/manual/en/faq.php      - PHP English FAQ
-    //https://www.php.net/manual/en/langref.php   - PHP English Language Reference
-
-    //3 links to resources that you can use to learn Git below
-    //https://docs.github.com/en/get-started/start-your-journey/git-and-github-learning-resources  - Git and GitHub Learning Resources
-
-    //https://docs.github.com/en/get-started/using-git  - Using Git
-
-    //https://git-scm.com/doc   - Git Documentation
-
-    //https://about.gitlab.com/blog/2022/09/14/git-resources-for-visual-learners/    - Git Resources for Visual Learners
-
-    //https://ohmygit.org/    - Oh My Git!
-
-    //https://eagain.net/articles/git-for-computer-scientists/   - Git for Computer Scientists
-
-    //https://github.com/pcottle/learnGitBranching#learnGitbranching   - Learn Git Branching
-
-    //https://www.youtube.com/watch?v=1ffBJ4sVUb4&t=125s   - Michael Schwern: Git for Ages 4 and Up
-    
-    //- Git Tutorial for Beginners: Command-Line Fundamentals
-    //This is a fantastic video of Michael G. Schwern at Linux conf.au in 2013. Using children's toys, Michael gives us a great example of what exactly goes on in Git. It’s an entertaining video with important basics and concepts for anyone struggling to understand Git.
-
-
-
-
 ?>
 <?php 
 //classes: 
 //ul1; nameTitle; gitRepoLink; phpResources; gitResources; littleLeft; looksGood; donutCounty; week1; week2; week3; week4; week5; week6; week7; week8; week9; week10;
-
-
 
 ?>
 
