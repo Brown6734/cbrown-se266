0336e39c625748880f6ef430f303307242dba086
Brown6734 <clbrown1@email.neit.edu>
2024-04-12 11:07:58 -0400

    Brown spring term 2024; finished week 2 site page assignment

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,16 @@
 
-<hr />
 
 
 <?php
-    echo 'this is my footer';
-    echo 'time stamp it later';
+    //echo 'this is my footer';
+    //echo 'time stamp it later';
 ?>
+
+<hr/>
+    <?php
+        $file = basename($_SERVER['PHP_SELF']);
+        $mod_date = date("F d Y h:i:s A", filemtime($file));
+        echo "File last updated $mod_date";
+        //got this from include/footer.php (example code on git hub)
+    ?>
+
--- site/index.php
+++ site/index.php
@@ -1,10 +1,39 @@
-<?php include __DIR__ . '/../include/header.php'; ?>
+<!--<//?php include __DIR__ . '/../include/header.php'; ?>-->
 
-<h1>Chloe Brown; my site </h1>
 
-<h2>smaller heading here </h2>
 
-<?php
+<!--<link rel="stylesheet" href="" type="text/css">-->
+
+
+
+<!--dd 
+    
+<style>
+<//?php include 'CSS/main.css'; ?>
+</style>
+-->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
+    <h1>Chloe Brown: My Site </h1>
+    
+    <!-- dd -->
+
+    <!--<h1>Chloe Brown; my site </h1>-->
+
+<!--<h2>smaller heading here </h2>-->
+
+ <?php
 
 //you need a web page that will serve as a centralized user interface for the projects in this course
 
@@ -68,35 +97,47 @@
 
 
 ?>
+<?php 
+//classes: 
+//ul1; nameTitle; gitRepoLink; phpResources; gitResources; littleLeft; looksGood; donutCounty; week1; week2; week3; week4; week5; week6; week7; week8; week9; week10;
 
-<ul>
-    <li>My Name Here</li>
-    <li><a href="linkhere">git hub repo link</a></li>
-    <li><a href="https://www.php.net/manual/en/">PHP English Manual</a></li>
-    <li><a href="https://www.php.net/manual/en/faq.php ">PHP English Frequently Asked Questions</a></li>
-    <li><a href="https://www.php.net/manual/en/langref.php">PHP English Language Reference</a></li>
-    <li><a href="https://about.gitlab.com/blog/2022/09/14/git-resources-for-visual-learners/">Git Resources for Visual Learners</a></li>
-    <li><a href="https://ohmygit.org/">Oh My Git! - A game about learning Git</a></li>
-    <li><a href="https://github.com/pcottle/learnGitBranching#learnGitbranching ">Learn Git Branching - A Visual and Interactive Way to Learn Git</a></li>
-    <li><a href="https://store.steampowered.com/app/1629520/A_Little_to_the_Left/">A Little to The Left (Cozy Puzzle/Organization Game)</a></li>
-    <li><a href="https://store.steampowered.com/app/2367190/Looks_Good/ ">Looks Good (Casual Puzzle Game)</a></li>
-    <li><a href="https://store.steampowered.com/app/702670/Donut_County/">Donut County (Meet the locals, steal their trash, and throw them in a hole)</a></li>
-    <li><a href="https://github.com/Brown6734/cbrown-se266/tree/3250fabcce3d18b1c914decd47a67624afad0868/week1">Week 1: ___</a></li>
-    <li><a href="https://github.com/Brown6734/cbrown-se266/tree/3250fabcce3d18b1c914decd47a67624afad0868/week2">Week 2: ____</a></li>
-    <li><a href="linkhere">week3 stuff link</a></li>
-    <li><a href="linkhere">week4 stuff link</a></li>
-    <li><a href="linkhere">week5 stuff link</a></li>
-    <li><a href="linkhere">week6 stuff link</a></li>
-    <li><a href="linkhere">week7 stuff link</a></li>
-    <li><a href="linkhere">week8 stuff link</a></li>
-    <li><a href="linkhere">week9 stuff link</a></li>
-    <li><a href="linkhere">week10 stuff link</a></li>
+
+
+?>
+
+    <ul class="ul1">
+        <li class="nameTitle">Chloe Brown</li>
+        <li class="gitRepoLink"><a href="https://github.com/Brown6734/cbrown-se266">My Git Hub Repo for SE-266</a></li>
+        <li class="phpResource1"><a href="https://www.php.net/manual/en/">PHP English Manual</a></li>
+        <li class="phpResource2"><a href="https://www.php.net/manual/en/faq.php ">PHP English Frequently Asked Questions</a></li>
+        <li class="phpResource3"><a href="https://www.php.net/manual/en/langref.php">PHP English Language Reference</a></li>
+        <li class="gitResource1"><a href="https://about.gitlab.com/blog/2022/09/14/git-resources-for-visual-learners/">Git Resources for Visual Learners</a></li>
+        <li class="gitResource2"><a href="https://ohmygit.org/">Oh My Git! - A game about learning Git</a></li>
+        <li class="gitResource3"><a href="https://github.com/pcottle/learnGitBranching#learnGitbranching ">Learn Git Branching - A Visual and Interactive Way to Learn Git</a></li>
+        <li class="littleLeft"><a href="https://store.steampowered.com/app/1629520/A_Little_to_the_Left/">A Little to The Left (Cozy Puzzle/Organization Game)</a></li>
+        <li class="looksGood"><a href="https://store.steampowered.com/app/2367190/Looks_Good/ ">Looks Good (Casual Puzzle Game)</a></li>
+        <li class="donutCounty"><a href="https://store.steampowered.com/app/702670/Donut_County/">Donut County (Meet the locals, steal their trash, and throw them in a hole)</a></li>
+        <li class="week1"><a href="https://github.com/Brown6734/cbrown-se266/tree/3250fabcce3d18b1c914decd47a67624afad0868/week1">Week 1 Assignments</a></li>
+        <li class="week2"><a href="https://github.com/Brown6734/cbrown-se266/tree/3250fabcce3d18b1c914decd47a67624afad0868/week2">Week 2 Assignments</a></li>
+        <li class="week3"><a href="https://github.com/Brown6734/cbrown-se266/tree/50e5954c2c76ef9c1610edaabf29d5e3c95290ef/week3">Week 3 Assignments</a></li>
+        <li class="week4"><a href="https://github.com/Brown6734/cbrown-se266/tree/50e5954c2c76ef9c1610edaabf29d5e3c95290ef/week4">Week 4 Assignments</a></li>
+        <li class="week5"><a href="https://github.com/Brown6734/cbrown-se266/tree/50e5954c2c76ef9c1610edaabf29d5e3c95290ef/week5">Week 5 Assignments</a></li>
+        <li class="week6"><a href="https://github.com/Brown6734/cbrown-se266/tree/50e5954c2c76ef9c1610edaabf29d5e3c95290ef/week6">Week 6 Assignments</a></li>
+        <li class="week7"><a href="https://github.com/Brown6734/cbrown-se266/tree/50e5954c2c76ef9c1610edaabf29d5e3c95290ef/week7">Week 7 Assignments</a></li>
+        <li class="week8"><a href="https://github.com/Brown6734/cbrown-se266/tree/50e5954c2c76ef9c1610edaabf29d5e3c95290ef/week8">Week 8 Assignments</a></li>
+        <li class="week9"><a href="https://github.com/Brown6734/cbrown-se266/tree/50e5954c2c76ef9c1610edaabf29d5e3c95290ef/week9">Week 9 Assignments</a></li>
+        <li class="week10"><a href="https://github.com/Brown6734/cbrown-se266/tree/50e5954c2c76ef9c1610edaabf29d5e3c95290ef/week10">Week 10 Assignments</a></li>
     
     
 
 
 
-</ul>
+    </ul>
 
 <?php include __DIR__ . '/../include/footer.php'; ?>
 
+
+
+
+</body>
+</html>
